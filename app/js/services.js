2b5ba3223f1b7b378a7e92f4fc3a989467ec35a8
jkanschik <jens_kanschik@freenet.de>
2013-07-28 17:43:19 +0200

    Refactored Wrapper to be used for other entities.

--- app/js/services.js
+++ app/js/services.js
@@ -2,15 +2,37 @@
 
 /* Services */
 
-var Wrapper = function(resource, rootScope) {
-
-}
+var Wrapper = function(resource, rootScope) {this.rootScope = rootScope; this.resource = resource};
+Wrapper.prototype.get = function(id, callback) {
+      if (this.rootScope.online) {
+        console.log("Wrapper 'get', online API access, id: ", id);
+        return this.resource.get({'_id': id}, function(doc) {
+          localStorage.setItem(id, JSON.stringify(doc));
+          callback(doc);
+        });
+      } else {
+        console.log("Wrapper 'get', offline access to local storage, id: ", id);
+        var localData = localStorage.getItem(id);
+        localData = JSON.parse(localData);
+        return localData;
+      }
+    };
+Wrapper.prototype.save = 
+    function(data) {
+      console.log("Wrapper 'save': storing data in local storage: ", data);
+      localStorage.setItem(data['_id'], JSON.stringify(data));
+      if (this.rootScope.online) {
+        console.log("Wrapper 'save', online API access, data: ", data);
+        this.resource.save(data);
+      }
+    };
 
 angular
   .module('myApp.services', ['ngResource'])
   .factory('Customer', ['$resource', '$rootScope',
     function($resource, $rootScope) {
-
+      var res = $resource('api/customers/:_id', {});
+      return new Wrapper(res, $rootScope);
     }
   ])
   .factory('RemoteLevel', ['$resource', '$http', '$rootScope',
@@ -24,32 +46,7 @@ angular
         'aggregate': {method:'PUT', params:{_id: 'aggregate'}, isArray:true}   
       }
       var res = $resource('api/levels/:_id', {}, actions);
-
-      var wrapper = {
-        get: function(id, callback) {
-          if ($rootScope.online) {
-            console.log("Wrapper 'get', online API access, id: ", id);
-            return res.get({'_id': id}, function(doc) {
-              localStorage.setItem(id, JSON.stringify(doc));
-              callback(doc);
-            });
-          } else {
-            console.log("Wrapper 'get', offline access to local storage, id: ", id);
-            var localData = localStorage.getItem(id);
-            localData = JSON.parse(localData);
-            return localData;
-          }
-        },
-        save: function(data) {
-          console.log("Wrapper 'save': storing data in local storage: ", data);
-          localStorage.setItem(data['_id'], JSON.stringify(data));
-          if ($rootScope.online) {
-            console.log("Wrapper 'save', online API access, data: ", data);
-            res.save(data);
-          }
-        }
-      };
-      return wrapper;
+      return new Wrapper(res, $rootScope);
     }
   ])
   .factory('LevelList', ['RemoteLevel',
