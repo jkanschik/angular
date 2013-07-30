12be89bacdd76a1223198742c19d46d52def440e
jkanschik <jens_kanschik@freenet.de>
2013-07-30 23:39:02 +0200

    Fixed parent handling

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -75,63 +75,93 @@ var controllers = {
     $scope.reset();
   },
 
+
+  /*
+
+  Attribute:
+  - parents: die vom Benutzer eingegebene Liste der Nummern.
+    Diese Liste muss 1:1 gespeichert werden, da die Referenzen später aufgelöst werden könnten.
+    Eine Auflösung in UUIDs hilft hier nicht.
+  - missingParents: der Teil der parents, der nicht aufgelöst werden kann und als Fehler angezeigt wird.
+
+  1. Eingabe der Parents durch den Benutzer
+  2. Umbenennung einer Messstellennummer (Eindeutigkeit bleibt erhalten)
+    a) die neue Nummer ist bei einer anderen Messstelle "missing"
+    b) die neue Nummer ist bei einer anderen Messstelle "parent", aber nicht "missing"
+    c) die alte Nummer ist bei einer anderen Messstelle "missing"
+    d) die alte Nummer ist bei einer anderen Messstelle "parent", aber nicht "missing"
+  3. Umbenennung einer Messstellennummer (Nummer ist bereits vergeben)
+
+  Zu 1)
+    Validierung starten.
+    location.$errors.missingParents = "Die folgenden Messstellennummern fehlen: " + Liste
+  Zu 2a)
+    Validierung neu starten, dadurch wird missingParents neu aufgebaut. Alternativ: neue Nummer aus missing entfernen.
+    In der neuen Liste der missingParents fehlt die neue Nummer.
+    Fehlermeldung wie 1)
+  Zu 2b)
+    Kann nicht sein, da dann die Nummer bereits vergeben sein muss.
+  Zu 2c)
+    Kann nicht sein, sonst wäre "missing" falsch gesetzt.
+  Zu 2d)
+    Die alte Nummer muss in parents durch den neuen Wert ersetzt werden.
+    Eine Neuvalidierung ist nicht nötig, da sich missingNumbers nicht ändert.
+  Zu 3)
+    Existenz der neuen Nummer prüfen + Validierungsmeldung.
+    location.$errors.number = "Die Messstellennummer ist bereits vergeben!"
+  */
   LocationsController: function($scope, $routeParams) {
     $scope.baseUrl = "/meteringConcepts/" + $routeParams._id;
-
-    $scope.resetLocationIdsByNumber = function() {
-      $scope.locationIdsByNumber = {};
-      angular.forEach($scope.locations, function(location) {
-        $scope.locationIdsByNumber[location.number] = location.id;
-      });
-    }
 
     /* Validate the parents of the location and set missingParents
      * to the list of parents which don't refer to an existing location.
      */
     $scope.validateParentLocations = function(location) {
-      console.log("Validating parents ", location.parentNumbers, " of location ", location.number);
+      console.log("Validating parents ", location.parents, " of location ", location.number);
 
-      location.parents = [];
-      location.missingParents = [];
+      location.missingParents = angular.copy(location.parents);
 
-      angular.forEach(location.parentNumbers, function(value) {
-        var parentId = $scope.locationIdsByNumber[value];
-        console.log("ParentId: ", parentId);
-        if (parentId)
-          location.parents.push(parentId);
-        else
-          location.missingParents.push(value);
+      angular.forEach($scope.locations, function(value) {
+        var ix = location.missingParents.indexOf(value.number);
+        if (ix > -1)
+          location.missingParents.splice(ix, 1);
         console.log(location.missingParents);
       });
-      // the result are the missing parents
     };
 
-    $scope.resetParentNumbers = function(location) {
-      location.parentNumbers = [];
-      angular.forEach(location.parents, function(parentId) {
-        var parentNumber = $scope.locations[parentId].number;
-        location.parentNumbers.push(parentNumber);
-      });
-    };
-
-    $scope.updateLocationNumber = function(location) {
-      $scope.resetLocationIdsByNumber();
-      angular.forEach($scope.locations, function(value) {
-//        $scope.resetParentNumbers(value);
-        $scope.validateParentLocations(value);
+    $scope.watchLocationNumber = function(location) {
+      $scope.$watch('locations.' + location.id + '.number', function(newValue, oldValue) {
+        if (oldValue == newValue)
+          return;
+        console.log("Location number has been changed from ", oldValue, " to ", newValue);
+        angular.forEach($scope.locations, function(value) {
+          // Fall 2a)
+          var ix = value.missingParents.indexOf(newValue);
+          if (ix > -1)
+            value.missingParents.splice(ix, 1);
+          // Fall 2d)
+          var ix = value.parents.indexOf(oldValue);
+          if (ix > -1) {
+            // Setting 'value.parents[ix] = newValue;' doesn't work; it doesn't update the input field
+            var newParents = angular.copy(value.parents);
+            newParents[ix] = newValue;
+            value.parents = newParents; // this is picked up by angular because the object itself is changed ;-)
+          }
+        });
       });
     };
 
 $scope.locations = {
   "id1": {id: "id1", number: "1.00", type: "MEZ", parents: []},
-  "id2": {id: "id2", number: "1.01", type: "MEZ", parents: ["id1"]},
-  "id3": {id: "id3", number: "1.02", type: "MEZ", parents: ["id1", "id2"]}
+  "id2": {id: "id2", number: "1.01", type: "MEZ", parents: ["1.00"]},
+  "id3": {id: "id3", number: "1.02", type: "MEZ", parents: ["1.01", "1.02"]}
 };
 
-$scope.resetLocationIdsByNumber();
 angular.forEach($scope.locations, function(location) {
-  $scope.resetParentNumbers(location);
+  $scope.validateParentLocations(location);
+  $scope.watchLocationNumber(location);
 });
+
 
 
   },
@@ -163,7 +193,7 @@ angular.forEach($scope.locations, function(location) {
 }
 
 angular
-  .module('myApp.controllers', ['ui.validate'])
+  .module('myApp.controllers', [])
   .controller('Welcome', ['$scope', '$location', 'MeteringConcept', 'Customer', controllers.WelcomeController])
   .controller('MeteringConcept', ['$scope', '$routeParams', 'MeteringConcept', controllers.MeteringConceptController])
   .controller('Customer', ['$scope', '$routeParams','Customer', controllers.CustomerController])
